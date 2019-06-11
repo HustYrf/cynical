20aa5d43b109e16659267245f9797ad5f6f74146
杨瑞丰 <aishan2@douyu.tv>
2019-06-11 17:31:18 +0800

    add House Robber --198

---
+++ src/main/java/HouseRobber/HouseRobber.java
@@ -0,0 +1,16 @@
+package HouseRobber;
+
+public class HouseRobber {
+    public int rob(int[] nums) {
+        if (nums.length == 0) return 0;
+        int[] dp = new int[nums.length+1];
+        dp[0] = 0;
+        dp[1] = nums[0];
+        int max = dp[1];
+        for (int i = 1; i < nums.length; i++) {
+            dp[i+1] = Math.max(dp[i], nums[i]+dp[i-1]);
+            max = Math.max(max,dp[i+1]);
+       }
+        return max;
+    }
+}
