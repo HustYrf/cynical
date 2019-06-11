57a02d181468519791142a340d6d784b2358e63c
杨瑞丰 <aishan2@douyu.tv>
2019-06-11 10:00:12 +0800

    add Maximum Subarray -53

---
+++ src/main/java/MaximumSubarray/MaximumSubarray.java
@@ -0,0 +1,16 @@
+package MaximumSubarray;
+
+
+import java.util.Arrays;
+
+public class MaximumSubarray {
+    public int maxSubArray(int[] nums) {
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+        }
+        Arrays.sort(dp);
+        return dp[dp.length-1];
+    }
+}
