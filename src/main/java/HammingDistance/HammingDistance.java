cd0b28ef24350185a9a072bcce2aba6ca80370bb
杨瑞丰 <aishan2@douyu.tv>
2019-06-20 16:50:34 +0800

    add Hamming Distance --461

---
+++ src/main/java/HammingDistance/HammingDistance.java
@@ -0,0 +1,11 @@
+package HammingDistance;
+
+public class HammingDistance {
+//    public int hammingDistance(int x, int y) {
+//        int i = x ^ y;
+//    }
+
+    public static void main(String[] args) {
+        System.out.println(1 ^ 4);
+    }
+}
