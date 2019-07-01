67c26692ad85ed7d72c754e4ede806fbd799b48f
杨瑞丰 <aishan2@douyu.tv>
2019-07-01 17:22:59 +0800

    add Rotate Image --48

---
+++ src/main/java/RotateImage/RotateImage.java
@@ -0,0 +1,23 @@
+package RotateImage;
+
+public class RotateImage {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        //先对角线互相翻转一下
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int tmp = matrix[j][i];
+                matrix[j][i] = matrix[i][j];
+                matrix[i][j] = tmp;
+            }
+        }
+        // 再左右反转一下
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - j - 1];
+                matrix[i][n - j - 1] = tmp;
+            }
+        }
+    }
+}
