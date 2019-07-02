7105c9eb3980f59c65255973b499473e45ca9fad
杨瑞丰 <aishan2@douyu.tv>
2019-07-02 15:07:42 +0800

    update Merge Intervals *****

--- src/main/java/MergeIntervals/MergeIntervals.java
+++ src/main/java/MergeIntervals/MergeIntervals.java
@@ -2,41 +2,52 @@ package MergeIntervals;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 //Input: [[1,3],[2,6],[8,10],[15,18]]
 //        Output: [[1,6],[8,10],[15,18]]
 //        Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
 public class MergeIntervals {
-    public int[][] merge(int[][] intervals) {
-        if (intervals.length <= 1)
-            return intervals;
-
-        // Sort by ascending starting point
-        Arrays.sort(intervals, (i1, i2) -> Integer.compare(i1[0], i2[0]));
-
-        List<int[]> result = new ArrayList<>();
-        int[] newInterval = intervals[0];
-        result.add(newInterval);
-        for (int[] interval : intervals) {
-            if (interval[0] <= newInterval[1]) // Overlapping intervals, move the end if needed
-                newInterval[1] = Math.max(newInterval[1], interval[1]);
-            else {                             // Disjoint intervals, add the new interval to the list
-                newInterval = interval;
-                result.add(newInterval);
-            }
+    public int[][] merge(int[][] arr) {
+        if (arr == null || arr.length <= 1) {
+            return arr;
         }
-
-        return result.toArray(new int[result.size()][]);
+        List<int[]> result = new ArrayList<>(); //import
+        Arrays.sort(arr, new DoubleCompare());
+        for (int i = 0; i < arr.length; i++) {
+            System.out.printf("%d,%d\n",arr[i][0],arr[i][1]);
+        }
+        int left = 0, right = 0;
+        int i = 0;
+        int len = arr.length;
+        while (i < len) {
+            left = arr[i][0];
+            right = arr[i][1];
+            while (i + 1 < len && right >= arr[i + 1][0]) {
+                right = Math.max(right, arr[i + 1][1]);
+                i++;
+            }
+            result.add(new int[]{left, right}); //import
+            i++;
+        }
+        int[][] resultArrs = result.toArray(new int[result.size()][2]);
+        return resultArrs;
     }
 
-//    public class DoubleCompare implements Comparator<Integer> {
-//        @Override
-//        public int compare(Integer o1, Integer o2) {
-//            if (o1 == o2) return 0;
-//            else {
-//                return o1 < 02 ? 1 : -1;
-//            }
-//        }
-//    }
+
+    private class DoubleCompare implements Comparator<int[]> {
+        @Override
+        public int compare(int[] o1, int[] o2) {
+            return o1[0] - o2[0];
+        }
+    }
+
+
+    public static void main(String[] args) {
+        //[[2,3],[4,5],[6,7],[8,9],[1,10]]
+        int[][] arr = new int[][]{{2, 3}, {4, 5}, {6, 7}, {8, 9}, {1, 10}};
+        MergeIntervals obj = new MergeIntervals();
+        obj.merge(arr);
+    }
 }
