d6a955b0557a68e8e3caf3e98b40cb49f1225e54
杨瑞丰 <aishan2@douyu.tv>
2019-07-03 15:36:27 +0800

    add LargestNumber --179 *

--- src/main/java/LargestNumber/LargestNumber.java
+++ src/main/java/LargestNumber/LargestNumber.java
@@ -1,4 +1,33 @@
 package LargestNumber;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class LargestNumber {
+    public String largestNumber(int[] nums) {
+        String[] asStrs = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            asStrs[i] = String.valueOf(nums[i]);
+        }
+        Arrays.sort(asStrs, new LargerNumberComparator());
+        if (asStrs[0].equals("0")) {
+            return "0";
+        }
+        String largestNumberStr = new String();
+        for (String numAsStr : asStrs) {
+            largestNumberStr += numAsStr;
+        }
+
+        return largestNumberStr;
+    }
+
+    private class LargerNumberComparator implements Comparator<String> {
+        @Override
+        public int compare(String a, String b) {
+            String order1 = a + b;
+            String order2 = b + a;
+            return order2.compareTo(order1);
+        }
+    }
+
 }
