2c55ac4e71c2414f40ca012dad4e2cb6ab4f005e
杨瑞丰 <aishan2@douyu.tv>
2019-07-09 21:27:40 +0800

    add Lru cache

---
+++ src/main/java/LRUCache/LRUCache.java
@@ -0,0 +1,105 @@
+package LRUCache;
+
+import java.util.*;
+
+/**
+ * Design and implement a data structure for Least Recently Used (LRU) cache. It should support the following operations: get and put.
+ * <p>
+ * get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
+ * put(key, value) - Set or insert the value if the key is not already present. When the cache reached its capacity, it should invalidate the least recently used item before inserting a new item.
+ * <p>
+ * The cache is initialized with a positive capacity.
+ */
+//使用双端链表加hashmap
+public class LRUCache {
+    private class DoubleNode {
+        int key;
+        int value;
+        DoubleNode pre;
+        DoubleNode next;
+
+        public DoubleNode(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private int size;
+    private int len;
+
+    private DoubleNode head;
+    private DoubleNode tail;
+    private Map<Integer, DoubleNode> map;
+
+    public LRUCache(int capacity) {
+        this.size = 0;
+        this.len = capacity;
+        map = new HashMap<>(capacity);
+    }
+
+    public int get(int key) {
+        DoubleNode doubleNode = getDoubleNode(key);
+        if (doubleNode == null) {
+            return -1;
+        }
+        removeExistNode(doubleNode);
+        addNodeToTail(doubleNode);
+        return doubleNode.value;
+    }
+
+    public void put(int key, int value) {
+        DoubleNode node = new DoubleNode(key, value);
+        DoubleNode doubleNode = getDoubleNode(key);
+
+        if (doubleNode == null) { //如果不存在
+            map.put(key, node);
+            addNodeToTail(node);
+            size++;
+            removeHeadNode(size);
+        } else {   //之前就已经存在,更新node，value
+            removeExistNode(doubleNode);
+            doubleNode.value = value;
+            addNodeToTail(doubleNode);
+        }
+    }
+
+    private void removeHeadNode(int size) {
+        if (size > len) {  //长度超标了，需要移除头节点
+            DoubleNode headCopy = head;
+            removeExistNode(head);
+            size--;
+            map.remove(headCopy.key);
+        }
+    }
+
+    private DoubleNode getDoubleNode(int key) {
+        DoubleNode doubleNode = map.get(key);
+        if (doubleNode == null) {
+            return null;
+        }
+        return doubleNode;
+    }
+
+    private void removeExistNode(DoubleNode node) {
+        if (node == head) {
+            head = head.next;
+            head.pre = null;
+        } else {
+            node.next.pre = node.pre;
+            node.pre.next = node.next;
+        }
+    }
+
+    private void addNodeToTail(DoubleNode node) {
+        if (tail == null) {
+            head = tail = node;
+        } else {
+            tail.next = node;
+            node.pre = tail;
+            tail = node;
+            tail.next = null;
+        }
+    }
+}
+
+
