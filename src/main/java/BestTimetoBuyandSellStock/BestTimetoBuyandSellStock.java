d8a73ddd6dab5d74e64ae1dbf96396af0ff0722b
杨瑞丰 <aishan2@douyu.tv>
2019-07-16 16:09:08 +0800

    update 121. Best Time to Buy and Sell Stock

--- src/main/java/BestTimetoBuyandSellStock/BestTimetoBuyandSellStock.java
+++ src/main/java/BestTimetoBuyandSellStock/BestTimetoBuyandSellStock.java
@@ -1,16 +1,20 @@
 package BestTimetoBuyandSellStock;
 
+// dp[i][k][0] = max(dp[i-1][k][0],dp[i-1][k][1]+prices[i])   第i天卖了股票
+// dp[i][k][1] = max(dp[i-1][k][1],dp[i-1][k-1][0]-prices[i]) 第i天买了股票
+// k = 1 只能交易一次
+//因为只能交易一次，所以在第i天有买了股票之前的天数里是不能买股票的，所有最大收益是就是0，因此dp[i-1][k-1][0] = 0;
+//消掉次数k，公式如下
+//dp[i][0] = max(dp[i-1][0],dp[i-1][1]+prices[i])
+//dp[i][1] = max(dp[i-1][1],-prices[i])
 public class BestTimetoBuyandSellStock {
     public int maxProfit(int[] prices) {
-        if (prices.length == 0) return 0;
+        if (prices.length <= 1) return 0;
         int max = 0;
-        int min = Integer.MAX_VALUE;
-        for (int i = 0; i < prices.length; i++) {
-            if (prices[i] < min) {
-                min = prices[i];
-            } else if (prices[i] - min > max) {
-                max = prices[i] - min;
-            }
+        int min = -prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            max = Math.max(max, min + prices[i]);
+            min = Math.max(min, -prices[i]);
         }
         return max;
     }
